4e6671a6fee5f85bd8058855ed996b313fcad5df
mymilk666 <58463443+mymilk666@users.noreply.github.com>
2021-03-03 17:26:49 -0800

    Update Get-ComputerInfo.ps1

--- Get-ComputerInfo.ps1
+++ Get-ComputerInfo.ps1
@@ -1,32 +1,6 @@
 
 
 function Get-ComputerInfo {
-
-    <#
-    .SYNOPSIS
-    Get Computer Information
-    
-    .DESCRIPTION
-    Get Computer information for remote computers
-    
-    .PARAMETER RemoteComputer
-    RemoteComputer
-    
-    .EXAMPLE
-    
-    Get-RemoteComputerInfo -RemoteComputer $env:COMPUTERNAME
-    
-    Get-RemoteComputerInfo -RemoteComputer 'Computername'
-    
-    run against several computers
-    $Data = import-csv c:\temp\servers.csv 
-    foreach($D in $Data){Get-RemoteComputerInfo -RemoteComputer $D.name}
-    
-    .NOTES
-    General notes
-    Disclaimer, run this at your own risk, supported AS-IS
-    
-    #>
     
     Param(
         [parameter(Mandatory=$true)]
